01cd9dabc77b58e8a74f6c5bfb95c8f0298d93c9
Rhaynerzin <rhayner.mendonca@iesb.edu.br>
2025-08-25 19:49:20 -0300

    criando projeto node e projeto calculadora de nota

---
+++ práticas/01-primeiro-projeto/index.js
@@ -0,0 +1,19 @@
+console.log("Meu primeiro projeto!!!")
+
+
+console.log("hello")
+
+let nome = 'Rhayner'
+
+let sobrenome = 'de Paiva'
+
+let idade = 22
+
+console.log("Olá" + nome + "" + sobrenome)
+console.log("Sua idade é" + idade)
+
+if(idade >= 18){
+    console.log("Você é maior de idade!!!")
+}else{
+    console.log("Você é maior de idade")
+}
