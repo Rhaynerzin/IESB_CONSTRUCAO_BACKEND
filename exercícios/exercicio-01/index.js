774fe9092cfdabc03a623639ee7bc8f39cb9b658
Rhaynerzin <rhayner.mendonca@iesb.edu.br>
2025-08-25 20:24:35 -0300

    Exercício 01

--- exercícios/exercicio-01/index.js
+++ exercícios/exercicio-01/index.js
@@ -1,9 +1,47 @@
+const prompt = require('prompt-sync')();
 const calc = require('./calculadora');
 
 console.log("=== Calculadora Node.js ===");
-console.log("3 + 4 =", calc.somar(3, 4));
-console.log("10 - 7 =", calc.subtrair(10, 7));
-console.log("6 * 5 =", calc.multiplicar(6, 5));
-console.log("8 / 2 =", calc.dividir(8, 2));
-console.log("5 ao quadrado =", calc.aoQuadrado(5));
-console.log("Raiz quadrada de 25 =", calc.raizQuadrada(25));
+
+console.log("Escolha a operação:");
+console.log("1 - Somar");
+console.log("2 - Subtrair");
+console.log("3 - Multiplicar");
+console.log("4 - Dividir");
+console.log("5 - Ao Quadrado");
+console.log("6 - Raiz Quadrada");
+
+let op = prompt("Digite o número da operação desejada: ");
+
+let resultado;
+
+if (op === "5") {
+  let num = Number(prompt("Digite um número: "));
+  resultado = calc.aoQuadrado(num);
+} else if (op === "6") {
+  let num = Number(prompt("Digite um número: "));
+  resultado = calc.raizQuadrada(num);
+} else {
+  let num1 = Number(prompt("Digite o primeiro número: "));
+  let num2 = Number(prompt("Digite o segundo número: "));
+  switch (op) {
+    case "1":
+      resultado = calc.somar(num1, num2);
+      break;
+    case "2":
+      resultado = calc.subtrair(num1, num2);
+      break;
+    case "3":
+      resultado = calc.multiplicar(num1, num2);
+      break;
+    case "4":
+      resultado = calc.dividir(num1, num2);
+      break;
+    default:
+      console.log("Operação inválida!");
+  }
+}
+
+if (resultado !== undefined) {
+  console.log("Resultado:", resultado);
+}
